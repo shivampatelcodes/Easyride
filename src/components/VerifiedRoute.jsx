c4f78b739b74fdb54140d388fb57acc49f408103
shivuptl06 <shivupatel9540@gmail.com>
2025-03-29 14:08:08 -0400

    Added Dropdown menu for Origin and Destination

--- src/components/VerifiedRoute.jsx
+++ src/components/VerifiedRoute.jsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import PropTypes from "prop-types";
 
 const VerifiedRoute = ({ children }) => {
   const auth = getAuth();
-  const user = auth.currentUser;
-  // If no user or if email is not verified, redirect to /verify-email
+  const [checking, setChecking] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+      setChecking(false);
+    });
+    return unsubscribe;
+  }, [auth]);
+
+  if (checking) {
+    return <div>Loading...</div>;
+  }
+
   if (!user) {
     return <Navigate to="/signin" replace />;
   }
+
   if (!user.emailVerified) {
     return <Navigate to="/verify-email" replace />;
   }
+
   return children;
 };
+
 VerifiedRoute.propTypes = {
   children: PropTypes.node.isRequired,
 };
